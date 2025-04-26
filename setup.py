27e28fa0ce62b742a3f9dbc3fca84b3f039fac59
Anthony <anthony.dopke@dauphine.eu>
2025-04-26 15:46:22 +0200

    setup modification

--- setup.py
+++ setup.py
@@ -2,7 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name="QuantWork",
-    version="0.1",
+    version="0.1.0",
+    description="A financial data and quant modeling library.",
+    author="Anthony Dopke",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
+    install_requires=[
+        "yfinance",
+        "pandas",
+        "numpy",
+        "matplotlib",
+        "scipy",
+    ],
+    python_requires=">=3.8",
 )
